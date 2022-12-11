9d39cbe33034ae8551f9c1b0951793d8ea73a2dc
AlefMemTav <190115548@aluno.unb.br>
2022-12-11 19:14:56 -0300

    Update

--- SQLQuery46.sql
+++ SQLQuery46.sql
@@ -1,6 +1,22 @@
 -- Nome dos funcionários com cargo de Designer Engineer
+/*
 SELECT *
 FROM Person.Person
 
-SELECT *
+SELECT BusinessEntityID
 FROM HumanResources.Employee
+WHERE JobTitle = 'Design Engineer'
+
+SELECT FirstName 
+FROM Person.Person
+WHERE BusinessEntityID IN
+(SELECT BusinessEntityID 
+FROM HumanResources.Employee
+WHERE JobTitle = 'Design Engineer')
+*/
+
+SELECT p.FirstName
+FROM Person.Person p
+INNER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID
+AND e.JobTitle = 'Design Engineer'
